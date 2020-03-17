82921516d855969950464166850b4a532a0006ef
bacchusjazz <bacchusjazz6248@gmail.com>
2020-03-18 08:02:03 +0900

    Scene1テキスト追加、仮セーブデータ作成機能追加開始

---
+++ GrassFlower_RPG/Assets/Scripts/BT/FadeIn.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class FadeIn : MonoBehaviour {
+    public GameObject Panel;
+    float a;
+
+    void Start () 
+    {
+        a = Panel.GetComponent<Image>().color.a;
+    }
+
+    void Update () 
+    {
+        StartCoroutine(FadePanel());
+    }
+
+    //フェードアウト自体は↓の処理
+    IEnumerator FadePanel()
+    {
+        while (a > 0)
+        {
+            a -= 0.01f;
+            Panel.GetComponent<Image>().color = new Color(255, 255, 255, a);
+            yield return null;
+        }
+    }
+    
+    
+}
---
+++ GrassFlower_RPG/Assets/Scripts/S1/FadeInText.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class FadeInText : MonoBehaviour {
+    public GameObject Text;
+    float a;
+
+    void Start () 
+    {
+        a = 0.0f;
+    }
+
+    void Update () 
+    {
+        StartCoroutine(FadeText());
+    }
+
+    //フェードアウト自体は↓の処理
+    IEnumerator FadeText()
+    {
+        while (a < 255)
+        {
+            a += 0.01f;
+            Text.GetComponent<Text>().color = new Color(0, 0, 0, a);
+            yield return null;
+        }
+    }
+    
+    
+}
