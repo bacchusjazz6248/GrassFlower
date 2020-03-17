aa292d547333270570200b2f44430df1152e287a
bacchusjazz <bacchusjazz6248@gmail.com>
2020-03-18 03:19:54 +0900

    タイトル画面追加

---
+++ GrassFlower_RPG/Assets/Scripts/FadeOut.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeOut : MonoBehaviour {
+    public GameObject Panel;
+    float a;
+
+    void Start () 
+    {
+        a = Panel.GetComponent<Image>().color.a;
+    }
+
+    //Aキーを押されたらフェード開始
+    void Update () 
+    {
+        StartCoroutine(FadePanel());
+    }
+
+    //フェードアウト自体は↓の処理
+    IEnumerator FadePanel()
+    {
+        yield return new WaitForSeconds(5);
+        while(a < 1)
+        {
+            Panel.GetComponent<Image>().color += new Color(0, 0, 0, 0.01f);
+            a += 0.01f;
+            yield return null;
+        }
+    }
+}
