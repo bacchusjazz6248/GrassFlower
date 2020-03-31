4c4bb83dd3c65b871e46c593dfe75b5d2e41ab2e
bacchusjazz <bacchusjazz6248@gmail.com>
2020-04-01 07:26:04 +0900

    キャラクターのステータス・コマンドクラス実装

---
+++ GrassFlower_RPG/Assets/Scripts/S2/BattleSystem.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleSystem : MonoBehaviour
+{
+	public CStatus player;
+	public CStatus enemy;
+	bool IsPlayerTurn;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		System.Random r = new System.Random(1000);
+		if (!IsPlayerTurn)
+		{
+			if (r.Next(1) == 0)
+			{
+				player.OnDamege(enemy.epower);
+				
+			}
+			else
+			{
+				player.SlimeBite(enemy.epower);
+			}
+		}
+	}
+
+	public void OnAttackButton()
+	{
+		enemy.OnDamege(player.upower);
+		IsPlayerTurn = false;
+	}
+	
+	public void OnFireButton()
+	{
+		if (player.mp < 30)
+		{
+			Debug.Log("MP不足！");
+			IsPlayerTurn = false;
+		}
+		else
+		{
+			player.UseMp(30);
+			enemy.OnDamege(player.upower*3);
+			IsPlayerTurn = false;
+		}
+	}
+	
+	public void OnHealButton()
+	{
+		if (player.mp < 30)
+		{
+			Debug.Log("MP不足！");
+			IsPlayerTurn = false;
+		}
+		else
+		{
+			player.UseMp(15);
+			player.OnHeal();
+			IsPlayerTurn = false;
+		}
+	}
+}
---
+++ GrassFlower_RPG/Assets/Scripts/S2/CStatus.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CStatus : MonoBehaviour
+{
+	public int hp;
+	int hpMax = 500;
+	public int mp;
+	int mpMax = 100;
+    public int upower;
+    public int epower;
+    
+	// Use this for initialization
+	void Start ()
+	{
+		hp = hpMax;
+		mp = mpMax;
+		upower = 100;
+		epower = 40;
+	}
+
+	public void OnDamege(int _damege)
+	{
+		hp -= _damege;
+	}
+	
+	public void OnHeal()
+	{
+		hp += 150;
+	}
+
+	public void UseMp(int use)
+	{
+		mp -= use;
+	}
+
+	public void SlimeBite(int _damege)
+	{
+		hp -= epower * 2;
+	}
+}
